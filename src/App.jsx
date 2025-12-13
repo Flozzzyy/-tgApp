7381d1bf1cf49a5d8b3c666c9a87d3d281e4905a
flfesof <flozzytiktok@gmail.com>
2025-12-13 09:01:55 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState, useRef } from "react";
+import products from "./data";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Item from "./pages/Item";
+const App = () => {
+  return (
+    <Routes>
+      <Route path={"/"} element={<Home />}></Route>
+      <Route path={"/item/:id"} element={<Item products={products} />}></Route>
+    </Routes>
+  );
+};
+
+export default App;
