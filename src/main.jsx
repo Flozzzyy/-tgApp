bc0d3929e7cec960256b2a3a40782374f6a37d1e
flozzyAbs <flozzytiktok@gmail.com>
2025-12-14 10:52:17 +0300

    app

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./context/ItemsContext.jsx";
 import Layout from "./pages/components/Layout.jsx";
+if (window.Telegram?.WebApp) {
+  window.Telegram.WebApp.ready();
+  window.Telegram.WebApp.expand();
+}
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
